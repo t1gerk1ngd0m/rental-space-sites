17b9d17614ef4d6fccd3bc7585710b930e951ecd
Taiga Miwa <taiga.miwa888@gmail.com>
2026-08-26 09:26:46 +0900

    OGP設定

--- smart-hometheater/astro.config.mjs
+++ smart-hometheater/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://smart-hometheater.com',
   compressHTML: false,
   trailingSlash: 'always',
   build: {
